09ab01f07107a65bb28dd6218e9bbe335e9807c9
Eyad Mubarak <eyad.mubarak@hotmail.com>
2022-05-11 20:26:24 +0300

    first push for spring boot and gamer fun site files

---
+++ gamersFun/src/main/java/gamersFun/com/example/gamersFun/GamersFunApplication.java
@@ -0,0 +1,13 @@
+package gamersFun.com.example.gamersFun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersFunApplication.class, args);
+	}
+
+}
---
+++ gamersFun/src/test/java/gamersFun/com/example/gamersFun/GamersFunApplicationTests.java
@@ -0,0 +1,13 @@
+package gamersFun.com.example.gamersFun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
