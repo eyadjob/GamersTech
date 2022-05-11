a42ce0d5c945df97976739fad8e9e25c362336b0
Odai Dahmos <odahmos@atypon.com>
2022-05-11 22:23:00 +0300

    Add theme to spring boot
    1- Add custom theme to spring boot
    
    2- disable thymeleafe since we using apache tail
    
    3- fixing dependancy

---
+++ gamersFun/src/main/java/gamersFun/com/example/gamersFun/contollers/PageController.java
@@ -0,0 +1,43 @@
+package gamersFun.com.example.gamersFun.contollers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+
+
+    @RequestMapping("/")
+    ModelAndView home(ModelAndView modelAndView){
+        modelAndView.setViewName("app.homepage");
+        modelAndView.getModel().put("statusUpdate","");
+        return modelAndView;
+    }
+
+    @RequestMapping("/about")
+    String about(){
+        return "app.about";
+    }
+
+    @RequestMapping("/contactUs")
+    String contactUs(){
+        return "app.contactUs";
+    }
+
+    @RequestMapping("/err")
+    String errr(){
+        if(true){
+            throw new RuntimeException("Error ");
+        }
+
+        return "app.contactUs";
+    }
+
+
+
+
+}
