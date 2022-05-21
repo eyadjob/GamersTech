2403ebdfdeaf043c6cd999e1aa72a6f4c7acf5f4
Eyad <eyadjob@yahoo.com>
2022-05-21 14:15:15 +0300

    major changes on project sturucture

--- gamersFun/backend/src/main/java/gamersFun/com/example/gamersFun/repository/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package gamersFun.com.example.gamersFun.repository;
-
-import gamersFun.com.example.gamersFun.entity.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BikeRepository extends JpaRepository<Bike, Long> {
-
-}
--- gamesFunSystemAngular/src/main/java/com/appress/ravi/userRegistrationSystem/bikeSystem/repositories/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appress.ravi.userRegistrationSystem.bikeSystem.repositories;
-
-
-import com.appress.ravi.userRegistrationSystem.bikeSystem.models.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BikeRepository extends JpaRepository<Bike,Long> {
-}
