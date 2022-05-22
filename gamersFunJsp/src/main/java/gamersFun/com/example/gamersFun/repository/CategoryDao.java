e8a82bf4173f76923feae090856f45155421a8b1
Eyad <eyadjob@yahoo.com>
2022-05-23 00:26:03 +0300

    add the admin page entities and repos

---
+++ gamersFunJsp/src/main/java/gamersFun/com/example/gamersFun/repository/CategoryDao.java
@@ -0,0 +1,11 @@
+package gamersFun.com.example.gamersFun.repository;
+
+
+import gamersFun.com.example.gamersFun.entity.CategoryEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryDao extends CrudRepository<CategoryEntity, Long> {
+    CategoryEntity findByEmail(String email);
+}
---
+++ gamersFunJsp/src/main/java/gamersFun/com/example/gamersFun/repository/NewsPageDao.java
@@ -0,0 +1,11 @@
+package gamersFun.com.example.gamersFun.repository;
+
+
+import gamersFun.com.example.gamersFun.entity.NewsPageEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsPageDao extends CrudRepository<NewsPageEntity, Long> {
+    NewsPageEntity findByEmail(String email);
+}
